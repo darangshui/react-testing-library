fd6e5d7d855694d0d4ad48c7fe384f5aa2a70b5f
darangshui <ohjamdol@gmail.com>
2023-01-15 13:05:35 +0900

    Test and code function to transform camel cse colors to sapced colors

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
+import { replaceCamelWithSpaces } from './App';
 
 test('button has correct initial color, and updates when clicked', () => {
   render(<App />);
@@ -144,4 +145,19 @@ test('Disabled button has gray background adn reverts to red', () => {
   // re-enable button
   fireEvent.click(checkbox);
   expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+});
+
+describe('spaces before camel-case capital letters', () => {
+  test('Works for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces('red')).toBe('red');
+  });
+
+  test('Works for one inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue');
+  });
+
+  test('Works for multiple inner capital letters', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red');
+  });
+
 });
