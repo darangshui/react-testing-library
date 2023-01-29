76250ea5ac5a006a1ca3c82a04be5971211e6fae
darangshui <ohjamdol@gmail.com>
2023-01-29 16:52:19 +0900

    Test and code alert for server error response2

--- sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
+++ sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
@@ -4,21 +4,20 @@ import { rest } from 'msw';
 import { server } from '../../../mocks/server';
 import { expect } from "@jest/globals";
 
-test('handles error for scoops and toppings routes', async () => {
+test("handles error for scoops and toppings routes", async () => {
   server.resetHandlers(
-    rest.get('http://localhost:3030/scoops', (req, res, ctx) => 
+    rest.get("http://localhost:3030/scoops", (req, res, ctx) =>
       res(ctx.status(500))
     ),
-    rest.get('http://localhost:3030/toppins', (req, res, ctx) => 
+    rest.get("http://localhost:3030/toppings", (req, res, ctx) =>
       res(ctx.status(500))
     )
   );
 
-  render(<OrderEntry />);
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
 
-  await waitFor( async () => {
-    const alerts = await screen.findAllByRole('alert');
+  await waitFor(async () => {
+    const alerts = await screen.findAllByRole("alert");
     expect(alerts).toHaveLength(2);
   });
-  
 });
